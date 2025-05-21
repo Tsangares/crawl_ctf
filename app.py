1f876a7080aef62cf7be12ab35be5ff074d36d2e
Tsangares <tsangares@gmail.com>
2025-05-21 02:39:28 +0200

    Updated

--- app.py
+++ app.py
@@ -434,4 +434,4 @@ def sync_players():
     return output
 
 if __name__=="__main__":
-    socketio.run(app,debug="true", host="0.0.0.0",port=8069)
+    socketio.run(app,debug="true", host="127.0.0.1",port=8069)
